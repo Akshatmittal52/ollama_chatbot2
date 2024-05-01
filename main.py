09dcc482a74469e02cb504ade9f2c5c9375067a5
Akshatmittal52 <90290751+Akshatmittal52@users.noreply.github.com>
2024-05-01 23:22:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -150,8 +150,8 @@ def setup_sidebar():
     st.sidebar.file_uploader(label="Upload new document", type=["pdf"], key="file_uploader",
                              on_change=handle_file_upload)
 
-    '''st.sidebar.file_uploader("Upload new document", type=["pdf"], key="file_uploader",
-                             on_change=handle_file_upload, label="Upload PDF file")'''
+    st.sidebar.file_uploader("Upload new document", type=["pdf"], key="file_uploader",
+                             on_change=handle_file_upload, label="Upload PDF file")
 
 
 def process_user_input():
